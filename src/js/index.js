392a4617e515cf20d48154ceec8ac2f3971cf9a9
Gao Sun <gaosu@cisco.com>
2017-03-21 14:17:40 -0700

    Expose Utility

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Nft from './notification';
 import Push from './push';
 import Progress from './progress';
 import Input from './input';
+import Utility from './utility';
 import SVG from 'svg/sprite.svg';
 
 // Add icons
@@ -15,4 +16,4 @@ window.addEventListener('load', () => {
 	document.body.insertBefore(dom, document.body.firstChild);
 });
 
-export { Window, Modal, Nft, Push, Progress, Input };
+export { Window, Modal, Nft, Push, Progress, Input, Utility };
